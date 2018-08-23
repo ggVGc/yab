d67f4abb21a184ce0e6941de392526421d22a6a8
Sundström Valter <mail@valter.nu>
2018-08-24 01:04:25 +0200

    Miner refactor

--- apps/miner/lib/miner/worker.ex
+++ apps/miner/lib/miner/worker.ex
@@ -55,6 +55,38 @@ defmodule Miner.Worker do
     sync()
   end
 
+  def status() do
+    GenServer.cast(__MODULE__, :status)
+  end
+
+  def set_account(new_public_key) when is_binary(new_public_key) do
+    GenServer.call(__MODULE__, {:set_account, new_public_key})
+  end
+
+  def get_blocks() do
+    GenServer.call(__MODULE__, :get_blocks)
+  end
+
+  def add_mined_block(%Block{} = mined_block, new_accounts) do
+    GenServer.cast(__MODULE__, {:add_mined_block, mined_block, new_accounts})
+  end
+
+  def add_external_block(%Block{} = new_block) do
+    GenServer.cast(__MODULE__, {:add_external_block, new_block})
+  end
+
+  def sync() do
+    GenServer.cast(__MODULE__, :sync)
+  end
+
+  def handle_call({:set_account, new_public_key}, _, state) do
+    {:reply, :ok, %State{state | public_key: new_public_key}}
+  end
+
+  def handle_call(:get_blocks, _, %State{blocks: blocks} = state) do
+    {:reply, blocks, state}
+  end
+
   def handle_info(:default_join, state) do
     # Bit ugly, but works for now
     {:ok, host} = :inet.gethostname()
@@ -69,35 +101,6 @@ defmodule Miner.Worker do
     {:noreply, state}
   end
 
-  def status() do
-    GenServer.cast(__MODULE__, :status)
-  end
-
-  def handle_cast(:status, %State{accounts: accounts, public_key: public_key} = state) do
-    Chain.get_account_balance(accounts, public_key)
-    |> IO.inspect(label: "balance")
-
-    public_key |> IO.inspect(label: "account", limit: :infinity)
-
-    {:noreply, state}
-  end
-
-  def set_account(new_public_key) when is_binary(new_public_key) do
-    GenServer.call(__MODULE__, {:set_account, new_public_key})
-  end
-
-  def handle_call({:set_account, new_public_key}, _, state) do
-    {:reply, :ok, %State{state | public_key: new_public_key}}
-  end
-
-  def get_blocks() do
-    GenServer.call(__MODULE__, :get_blocks)
-  end
-
-  def handle_call(:get_blocks, _, %State{blocks: blocks} = state) do
-    {:reply, blocks, state}
-  end
-
   def handle_info(:mine, %State{active_mining_task: active_mining_task})
       when not is_nil(active_mining_task),
       do: raise("mining task already active")
@@ -106,7 +109,7 @@ defmodule Miner.Worker do
         :mine,
         %State{
           accounts: accounts,
-          blocks: [latest_block | _] = blocks,
+          blocks: [latest_block | _],
           public_key: public_key
         } = old_state
       ) do
@@ -123,7 +126,7 @@ defmodule Miner.Worker do
 
     task =
       Task.Supervisor.async(Miner.TaskSupervisor, fn ->
-        mine(candidate, new_accounts, blocks)
+        mine(candidate, new_accounts)
       end)
 
     new_state = %{old_state | active_mining_task: task}
@@ -131,7 +134,7 @@ defmodule Miner.Worker do
     {:noreply, new_state}
   end
 
-  defp mine(candidate, new_accounts, blocks) do
+  defp mine(candidate, new_accounts) do
     Logger.info("Mining block")
 
     mined_block = %{candidate | header: POW.work(candidate.header)}
@@ -142,98 +145,13 @@ defmodule Miner.Worker do
     add_mined_block(mined_block, new_accounts)
   end
 
-  def add_mined_block(%Block{} = mined_block, new_accounts) do
-    GenServer.cast(__MODULE__, {:add_mined_block, mined_block, new_accounts})
-  end
+  def handle_cast(:status, %State{accounts: accounts, public_key: public_key} = state) do
+    Chain.get_account_balance(accounts, public_key)
+    |> IO.inspect(label: "balance")
 
-  def handle_cast(
-        {:add_mined_block, mined_block, new_accounts},
-        %State{
-          blocks: blocks,
-          active_mining_task: active_mining_task,
-          public_key: public_key
-        } = old_state
-      ) do
-    # Make sure task actually finished
-    _ = Task.await(active_mining_task)
+    public_key |> IO.inspect(label: "account", limit: :infinity)
 
-    new_state = %State{
-      old_state
-      | accounts: new_accounts,
-        blocks: [mined_block | blocks],
-        active_mining_task: nil
-    }
-
-    Logger.debug("Balance: #{Chain.get_account_balance(new_accounts, public_key)}")
-
-    schedule_mining()
-
-    {:noreply, new_state}
-  end
-
-  def add_external_block(%Block{} = new_block) do
-    GenServer.cast(__MODULE__, {:add_external_block, new_block})
-  end
-
-  def handle_cast(
-        {:add_external_block, %Block{} = new_block},
-        %State{
-          accounts: accounts,
-          blocks: [latest_block | _] = blocks,
-          active_mining_task: active_mining_task
-        } = state
-      ) do
-    Logger.info("Received external block")
-
-    case try_add_block(0, new_block, state) do
-      {:ok, new_account_balances, new_blocks} ->
-        kill_mining_task_if_active(state)
-
-        new_state = %State{
-          state
-          | accounts: new_account_balances,
-            blocks: new_blocks,
-            active_mining_task: nil
-        }
-
-        schedule_mining()
-        {:noreply, new_state}
-
-      {:error, reason} ->
-        Logger.info("Received invalid(#{reason}) block: #{inspect(new_block.header)}")
-        {:noreply, state}
-    end
-  end
-
-  defp try_add_block(
-         tries,
-         %Block{} = new_block,
-         %State{
-           accounts: accounts,
-           blocks: [latest_block | rest_blocks] = blocks,
-           active_mining_task: active_mining_task
-         } = state
-       ) do
-    case Chain.validate_block(latest_block, accounts, new_block) do
-      {:ok, new_account_balances} ->
-        {:ok, new_account_balances, [new_block | blocks]}
-
-      {:error, _} = error ->
-        if tries >= @max_block_try_depth or rest_blocks == [] do
-          error
-        else
-          try_add_block(tries + 1, new_block, %State{state | blocks: rest_blocks})
-        end
-    end
-  end
-
-  defp schedule_mining() do
-    Logger.debug("Scheduling mining")
-    Process.send_after(__MODULE__, :mine, @initial_delay)
-  end
-
-  def sync() do
-    GenServer.cast(__MODULE__, :sync)
+    {:noreply, state}
   end
 
   def handle_cast(:sync, %State{} = state) do
@@ -269,10 +187,83 @@ defmodule Miner.Worker do
      }}
   end
 
+  def handle_cast(
+        {:add_mined_block, mined_block, new_accounts},
+        %State{
+          blocks: blocks,
+          active_mining_task: active_mining_task,
+          public_key: public_key
+        } = old_state
+      ) do
+    # Make sure task actually finished
+    _ = Task.await(active_mining_task)
+
+    new_state = %State{
+      old_state
+      | accounts: new_accounts,
+        blocks: [mined_block | blocks],
+        active_mining_task: nil
+    }
+
+    Logger.debug("Balance: #{Chain.get_account_balance(new_accounts, public_key)}")
+
+    schedule_mining()
+
+    {:noreply, new_state}
+  end
+
+  def handle_cast({:add_external_block, %Block{} = new_block}, %State{} = state) do
+    Logger.info("Received external block")
+
+    case try_add_block(new_block, state) do
+      {:ok, new_account_balances, new_blocks} ->
+        kill_mining_task_if_active(state)
+
+        new_state = %State{
+          state
+          | accounts: new_account_balances,
+            blocks: new_blocks,
+            active_mining_task: nil
+        }
+
+        schedule_mining()
+        {:noreply, new_state}
+
+      {:error, reason} ->
+        Logger.info("Received invalid(#{reason}) block: #{inspect(new_block.header)}")
+        {:noreply, state}
+    end
+  end
+
   defp kill_mining_task_if_active(%State{active_mining_task: nil}), do: false
 
   defp kill_mining_task_if_active(%State{active_mining_task: active_mining_task}) do
     Logger.info("Killing active mining task")
     Task.shutdown(active_mining_task, :brutal_kill)
+  end
+
+  defp try_add_block(new_block, state), do: try_add_block(0, new_block, state)
+
+  defp try_add_block(
+         tries,
+         %Block{} = new_block,
+         %State{accounts: accounts, blocks: [latest_block | rest_blocks] = blocks} = state
+       ) do
+    case Chain.validate_block(latest_block, accounts, new_block) do
+      {:ok, new_account_balances} ->
+        {:ok, new_account_balances, [new_block | blocks]}
+
+      {:error, _} = error ->
+        if tries >= @max_block_try_depth or rest_blocks == [] do
+          error
+        else
+          try_add_block(tries + 1, new_block, %State{state | blocks: rest_blocks})
+        end
+    end
+  end
+
+  defp schedule_mining() do
+    Logger.debug("Scheduling mining")
+    Process.send_after(__MODULE__, :mine, @initial_delay)
   end
 end
